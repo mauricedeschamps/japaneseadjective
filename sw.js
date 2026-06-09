11f4d6ea660a530f3a6d5d29642179fb2289f8f7
mauricedeschamps <mementomorikatsu@gmail.com>
2026-06-09 17:08:48 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -7,7 +7,6 @@ const urlsToCache = [
   'manifest.json',
   'icons/icon-192.png',
   'icons/icon-512.png'
-  
 ];
 
 self.addEventListener('install', event => {
@@ -19,5 +18,15 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
   );
 });
